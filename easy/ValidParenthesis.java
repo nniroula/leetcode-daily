eff7ed9b7c7ec73e3abcb5f480fa4083cf9ada1f
nabin <nkbhaie2017@gmail.com>
2026-04-01 13:31:33 -0600

    Valid Parenthesis

---
+++ easy/ValidParenthesis.java
@@ -0,0 +1,21 @@
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+*/
+
+class Solution {
+    public boolean isValid(String s) {
+        boolean validParenthesis = false;
+        // use stack class from collections framework to track parenthesis
+        Stack<String> stack;
+        Stack stack = new Stack<>();
+
+        return validParenthesis;
+    }
+}
+
+public class ValidParenthesis{
+    public static void main(String[] args) {
+        System.out.println("Valid Parenthesis..");
+
+    }
+}
